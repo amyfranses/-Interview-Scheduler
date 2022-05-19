e91ed3705ce4aa3c29633c02e18bb4168970af5e
amyfranses <amyfranses@gmail.com>
2022-05-19 13:33:04 -0700

    create InterviewerListItem Component,add scss

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  const interviewerImgClass = classNames("interviewers__item-image", {
+    "interviewers__item-image--selected": props.selected,
+  });
+
+  return (
+    <li
+      className={interviewerClass}
+      onClick={() => props.setInterviewer(props.id)}
+    >
+      <img
+        className={interviewerImgClass}
+        src={props.avatar}
+        alt="{props.name}"
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
